0a5c9600adea449b3d275b70ba38428f45b7c67c
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-03 17:22:07 -0400

    Imports 'Route' and 'NavLink' from 'react-router-dom' and places two <NavLink> elements inside of the <nav> and two <Route> elements inside of the <main>

--- client-side-auth/src/App.js
+++ client-side-auth/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route, NavLink } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
@@ -7,11 +8,14 @@ class App extends Component {
       <div className="App">
         <header>
           <nav>
-
+            <NavLink to="/login">Login</NavLink>
+            &nbsp;|&nbsp;
+            <NavLink to="/users">Users</NavLink>
           </nav>
         </header>
         <main>
-
+          <Route path="/login" component={Login}></Route>  
+          <Route path="/users" component={Users}></Route>  
         </main>
       </div>
     );
