6b2e70b72c513ffda3203f1396000a8f1767ba62
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-03 18:24:20 -0400

    Creates a .map() function to map over the list of users from the database

--- client-side-auth/src/users/Users.js
+++ client-side-auth/src/users/Users.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
 class Users extends React.Component {
+    state = {
+        users: []
+    }
+
     render() {
         return (
             <div>
                 <h2>List of Users</h2>
+                <ul>
+                    {this.state.users.map(u => (
+                        <li key={u.id}>{u.username}</li> 
+                    ))}
+                </ul>
             </div>
         );
     }
