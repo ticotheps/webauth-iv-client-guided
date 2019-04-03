7826cbf8866a6756567ee4a1c853d6d2334d6ceb
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-03 19:15:00 -0400

    Creates a <form> element inside of <Login /> that includes <input> elements for 'username' and 'password'

--- client-side-auth/src/login/Login.js
+++ client-side-auth/src/login/Login.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React from "react";
 
 class Login extends React.Component {
-    render() {
-        return (
-            <div>
-                <h2>Login</h2>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h2>Login</h2>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label htmlFor="" />
+            <input
+              name="username"
+              id="username"
+              value={this.state.username}
+              onChange={this.handleInputChange}
+              type="text"
+            />
+          </div>
+          <div>
+            <label htmlFor="" />
+            <input
+              name="password"
+              id="password"
+              value={this.state.password}
+              onChange={this.handleInputChange}
+              type="text"
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Login;
